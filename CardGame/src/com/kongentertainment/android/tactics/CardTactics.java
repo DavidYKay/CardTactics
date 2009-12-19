bd0f9b07e48a25e478994352a72ed1dfabf76451
David Y. Kay <david.kay@icihere.com>
2009-12-18 22:14:46 -0500

    Huge import. New directory structure, etc.

---
+++ CardGame/src/com/kongentertainment/android/tactics/CardTactics.java
@@ -0,0 +1,13 @@
+package com.kongentertainment.android.tactics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardTactics extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
