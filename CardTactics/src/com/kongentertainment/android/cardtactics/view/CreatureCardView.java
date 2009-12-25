bd269d51b5dfe9e2d1640dd2d4f8aefda5b0ac2b
David Young-Chan Kay <dk@miniKong.(none)>
2009-12-25 02:19:36 -0500

    Added some touch detection to CreatureCardView. Need to plug it into the view and test.

--- CardTactics/src/com/kongentertainment/android/cardtactics/view/CreatureCardView.java
+++ CardTactics/src/com/kongentertainment/android/cardtactics/view/CreatureCardView.java
@@ -1,5 +1,82 @@
 package com.kongentertainment.android.cardtactics.view;
 
+import com.kongentertainment.android.cardtactics.model.entities.CreatureCard;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.GestureDetector.SimpleOnGestureListener;
+
 public class CreatureCardView extends CardView {
 
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    MyGestureDetector mGestureDetector;
+
+    public CreatureCardView(CreatureCard card) {
+        super(card);
+
+        //mGestureDetector = new GestureDetector(new MyGestureDetector());
+
+        /*
+        setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				Log.d("CreatureCardView", "Touch event detected!");
+				return false;
+			}        	
+        });
+        */
+    }
+ 
+    //Detect touch
+
+    class MyGestureDetector extends SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                //X swipe
+                if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH) {
+                    return false;
+                }
+                //Swipe down
+                if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    
+                //Swipe up
+                }  else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                
+                }
+                /*
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	viewFlipper.setInAnimation(slideLeftIn);
+                    viewFlipper.setOutAnimation(slideLeftOut);
+                	viewFlipper.showNext();
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	viewFlipper.setInAnimation(slideRightIn);
+                    viewFlipper.setOutAnimation(slideRightOut);
+                	viewFlipper.showPrevious();
+                }
+                */
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+    }
+    
+    /*
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event))
+	        return true;
+	    else
+	    	return false;
+    }
+    */
+
+    
 }
